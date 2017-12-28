b193cfc24346806494bb825612e7f1e0b39c126c
Jmoorhead <Jeff.Moorhead1@gmail.com>
2017-12-28 15:20:10 -0500

    Added timing functionality.

--- list_primes.py
+++ list_primes.py
@@ -1,14 +1,18 @@
-from textwrap import fill
+import time
 import prime_functions as pf
 
 number = int(input("Find primes up to what number? "))
 # Save primes to a list.
+start = time.time()
 primes = pf.list_primes(number)
+end = time.time()
 
 for prime in primes[0:len(primes)-1]:
     print(prime, end=', ')
 print(primes[len(primes)-1])
 # Print length of primes.
 print(f'Number of primes less than {number}: {len(primes)}')
+# Display total time of calculation.
+print(f'The calculation took {end-start} seconds.')
 # Pause before exiting.
 input()
